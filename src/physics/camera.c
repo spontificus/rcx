061540427c85797eafac5fa0b9ff8b1033f76703
slinger <mats@rtv1.localdomain>
2009-10-07 16:25:32 +0200

    movement into colliding object is removed

--- src/physics/camera.c
+++ src/physics/camera.c
@@ -13,12 +13,24 @@ void camera_physics_step(dReal step)
 
 		int i;
 		dReal depth;
+		dReal V;
 		for (i=0; i<count; ++i)
 		{
 			depth = contact[i].depth;
 			camera.pos[0]-=contact[i].normal[0]*depth;
 			camera.pos[1]-=contact[i].normal[1]*depth;
 			camera.pos[2]-=contact[i].normal[2]*depth;
+
+			//remove movement into colliding object
+			//1) velocity along collision axis
+			V = camera.vel[0]*contact[i].normal[0] + camera.vel[1]*contact[i].normal[1] + camera.vel[2]*contact[i].normal[2];
+			if (V > 0) //along right direction?
+			{
+				//remove direction
+				camera.vel[0]-=V*contact[i].normal[0];
+				camera.vel[1]-=V*contact[i].normal[1];
+				camera.vel[2]-=V*contact[i].normal[2];
+			}
 		}
 	}
 }
