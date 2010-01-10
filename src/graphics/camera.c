e0b9962cb4dc3ff1950dac06bbe33c1e6dd154e3
slinger <mats@rtv1.localdomain>
2010-01-10 13:50:55 +0100

    making sure mode switching doesn't cause problems (like: stuck in reverse or air mode, when new setting disabled it)

--- src/graphics/camera.c
+++ src/graphics/camera.c
@@ -20,6 +20,17 @@ void set_camera_settings (camera_settings *settings)
 				dGeomDestroy(camera.geom);
 			camera.geom=NULL;
 		}
+
+		//if this camera mode doesn't have reverse enabled, make sure camera isn't stuck in reverse
+		if (!settings->reverse)
+			camera.reverse = false;
+
+		//if this camera mode has "air mode" disabled, make sure camera isn't stuck in "in air" mode from before
+		if (!settings->in_air)
+		{
+			camera.in_air = false;
+			camera.offset_scale = 1;
+		}
 	}
 }
 
