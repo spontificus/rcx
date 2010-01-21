d3779dd92c7d02b06556e7564e7fd33d69a96184
slinger <mats@rtv1.localdomain>
2010-01-21 20:00:42 +0100

    changed issue message (now contained in external string)

--- src/main.c
+++ src/main.c
@@ -17,6 +17,33 @@
 
 #define VERSION "0.06 (NOT DONE)" //supports alphanumeric versioning
 
+const char ISSUE[] =
+"    RollCageX  Copyright (C) 2009-2010  \"Slinger\" (on gorcx.net forum)\n\n\
+   This program is free software: you can redistribute it and/or modify\n\
+   it under the terms of the GNU General Public License as published by\n\
+   the Free Software Foundation, either version 3 of the License, or\n\
+   (at your option) any later version.\n\n\
+   This program comes with ABSOLUTELY NO WARRANTY:\n\n\
+   This program is distributed in the hope that it will be useful,\n\
+   but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+   GNU General Public License for more details.\n\n\
+   You should have received a copy of the GNU General Public License\n\
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\n\
+    = Credits =\n\
+      * \"Slinger\"	Creator (coder)\n\
+      * \"XEWEASEL\"	3D Models\n\n\
+    = Special Thanks =\n\
+      * \"K.Mac\"		Extensive testing and new ideas\n\
+      * \"MeAkaJon\"	Creator/maintainer of gorcx.net webpage\n\
+      * \"Spontificus\"	Testing, hacks, github registion and various fixes\n\n\
+    = Other Projects that made RCX possible =\n\
+      * \"Simple DirectMedia Layer\"	OS/hardware abstractions\n\
+      * \"Open Dynamics Engine\"	Rigid body dynamics and collision detection\n\
+      * \"The GNU Project\"		Its fight for computer freedom has changed the world\n\n\
+    - See README for more info -\n\n";
+   
+
 //Required stuff:
 #include <SDL.h>
 #include <SDL_opengl.h>
@@ -77,32 +104,8 @@ void emergency_quit (void)
 int main (int argc, char *argv[])
 {
 	//issue
-	printf("\n    -=[ Hello, and welcome to RollCageX version %s ]=-\n\n", VERSION);
-	printf(" Copyright (C) 2009, This program comes with ABSOLUTELY NO WARRANTY; see\n \"license.txt\" for details\n\n");
-	printf(" This is free software, and you are welcome to redistribute it and/or modify\n it under the GNU General Public License as published by the Free Software\n Foundation, version 3 or (at your option) later\n\n");
-
-	printf("= Credits (nicknames refers to usernames on the gorcx.net forum):\n");
-	printf("    \"MaAkaJon\"\t\tproject creator\n");
-	printf("    \"Soul Slinger\"\tcoder (created this and the earlier versions)\n");
-	printf("    All of you on the planetrollcage.com and gorcx.net forums!\n\n");
-
-	printf("* Projects that made RCX possible:\n");
-	printf("    GNU\t\t\t\tdefines computer freedom itself... :-)\n");
-	printf("    Simple DirectMedia Layer\twindow handling, input/outputs\n");
-	printf("    Open Dynamics Engine\trigid body dynamics\n");
-	printf("= End of credits\n\n");
-
-	printf(" Default controls (can be changed in profile):\n");
-	printf("	>	Arrow keys:	Steering and throttling\n");
-	printf("	>	Spacebar:	Drifting break\n");
-	printf("	>	Left Alt:	Soft breaks\n");
-	printf("	>	Q and E:	change camera distance along Z axis\n");
-	printf("	>	A and D:	change camera distance along X axis\n");
-	printf("	>	W and S:	change camera distance along Y axis\n");
-	printf("	>	F1 to F4:	change camera settings\n");
-	printf("	>	F5:		spawn box\n");
-	printf("	>	F6:		spawn box (10s above ground)\n\n");
-	//end of issue
+	printf("\n     -=[ Hello, and welcome to RollCageX version %s ]=-\n\n%s\n", VERSION, ISSUE);
+	//end
 
 	if (argc != 1)
 		printf("(Passing arguments - not supported)\n\n");
