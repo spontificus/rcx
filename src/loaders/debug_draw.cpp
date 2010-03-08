e0f7b8ca24b78144a48b2d1eeea2b9476fb40032
slinger <slinger@localdomain>
2010-03-08 14:22:51 +0100

    Changed sphere rendering generation code...

--- src/loaders/debug_draw.cpp
+++ src/loaders/debug_draw.cpp
@@ -3,6 +3,8 @@
 #include "colours.hpp"
 #include "../shared/printlog.hpp"
 
+#include <math.h>
+
 void debug_draw_box (GLuint list, GLfloat x, GLfloat y, GLfloat z,
 		const GLfloat colour[], const GLfloat specular[], const GLint shininess)
 {
@@ -78,7 +80,7 @@ void debug_draw_sphere_part(GLfloat x, GLfloat y, GLfloat z)
 void debug_draw_sphere (GLuint list, GLfloat d, const GLfloat colour[],
 		const GLfloat specular[], const GLint shininess)
 {
-	printlog(2, "Creating rendering list for debug sphere");
+	printlog(2, " > Creating rendering list for debug sphere\n");
 	GLfloat radius = d/2;
 
 	glNewList (list, GL_COMPILE);
@@ -88,20 +90,43 @@ void debug_draw_sphere (GLuint list, GLfloat d, const GLfloat colour[],
 	glMaterialfv (GL_FRONT, GL_SPECULAR, specular);
 	glMateriali (GL_FRONT, GL_SHININESS, shininess);
 
-	glBegin (GL_TRIANGLES);
-	debug_draw_sphere_part(-radius	,radius		,radius);
-	debug_draw_sphere_part(radius	,radius		,radius);
-	debug_draw_sphere_part(-radius	,-radius	,radius);
-	debug_draw_sphere_part(radius	,-radius	,radius);
-	debug_draw_sphere_part(-radius	,radius		,-radius);
-	debug_draw_sphere_part(radius	,radius		,-radius);
-	debug_draw_sphere_part(-radius	,-radius	,-radius);
-	debug_draw_sphere_part(radius	,-radius	,-radius);
-	glEnd();
+	// normals still not perfect, but then it's an imperfect algorithm
+	int slices = 12;
+	int divisions = 12;
+	int i,j,ii,jj;
+
+	float slp = M_PI/slices;
+	float sld = 2.0*M_PI/divisions;
+	float dx,dy,dz;
+
+	for (i = 0; i < slices; i++ ) {
+		ii = i + 1;
+		glBegin (GL_TRIANGLE_STRIP);
+		for (j = 0; j <= divisions; j++ ) {
+			jj = j + 1;
+			
+			dx = sin(slp * i) * sin (sld * j);
+			dy = sin(slp * i) * cos (sld * j);
+			dz = -cos(slp*i);
+			//printf("dx:%f dy:%f dz:%f\n", dx, dy, dz);
+			glNormal3f(dx, dy, dz);
+			glVertex3f( radius * dx, radius * dy, radius * dz );
+			
+			dx = sin(slp * ii) * sin (sld * jj);
+			dy = sin(slp * ii) * cos (sld * jj);
+			dz = -cos(slp*ii);
+			//printf("2dx:%f dy:%f dz:%f\n", dx, dy, dz);
+			glNormal3f(dx, dy, dz);
+			glVertex3f( radius * dx, radius * dy, radius * dz );
+			
+		}
+		glEnd();
+	}
+	
 
 	glMaterialfv (GL_FRONT, GL_SPECULAR, black);
 
 	glDisable(GL_NORMALIZE);
 	glEndList();
-}
 
+}
