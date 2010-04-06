f68cb6ee944adb7962d5bed6fc80f46d9342e54e
slinger <mats@ARCH.localdomain>
2010-04-06 05:26:31 +0200

    new approach for event lists not using template anymore: overloading functions instead rather ugly in some ways (~repeated code), but overall cleaner solution

--- src/events/event_lists.cpp
+++ src/events/event_lists.cpp
@@ -1,18 +1,134 @@
 #include "event_lists.hpp"
 
-//make sure header pointers are NULL for the wanted classes
-template<> Event_List<Geom> *Event_List<Geom>::head = NULL;
-template<> Event_List<Body> *Event_List<Body>::head = NULL;
-template<> Event_List<Joint> *Event_List<Joint>::head = NULL;
+//make sure header pointers are NULL for the wanted lists
+Event_Lists *Event_Lists::geom_head = NULL;
+Event_Lists *Event_Lists::body_head = NULL;
+Event_Lists *Event_Lists::joint_head = NULL;
 
-template<typename T> Event_List<T>::Event_List(T *component)
+//for adding to lists
+Event_Lists::Event_Lists(Geom *geom): component(geom)
 {
+	next = geom_head;
+	geom_head = this;
 }
 
-template<typename T> T *Event_List<T>::Get_Event()
+Event_Lists::Event_Lists(Body *body): component(body)
 {
+	next = body_head;
+	body_head = this;
 }
 
-template<typename T> void Event_List<T>::Remove(T *component)
+Event_Lists::Event_Lists(Joint *joint): component(joint)
 {
+	next = joint_head;
+	joint_head = this;
 }
+
+//for getting
+bool Event_Lists::Get_Event(Geom **geom)
+{
+	//if end of list
+	if (!geom_head)
+		return false;
+
+	//remove from list
+	Event_Lists *tmp = geom_head;
+	geom_head = tmp->next;
+
+	//set wanted data and delete
+	*geom = (Geom*)tmp->component; //safe to assume this list consists of wanted component
+	delete (tmp);
+
+	return true;
+}
+
+bool Event_Lists::Get_Event(Body **body)
+{
+	if (!body_head)
+		return false;
+
+	Event_Lists *tmp = body_head;
+	body_head = tmp->next;
+
+	*body = (Body*)tmp->component;
+	delete (tmp);
+
+	return true;
+}
+
+bool Event_Lists::Get_Event(Joint **joint)
+{
+	if (!joint_head)
+		return false;
+
+	Event_Lists *tmp = joint_head;
+	joint_head = tmp->next;
+
+	*joint = (Joint*)tmp->component;
+	delete (tmp);
+
+	return true;
+}
+
+//removing
+void Event_Lists::Remove(Geom *comp)
+{
+	Event_Lists *p = geom_head; //points at current event in list
+	Event_Lists **pp = &geom_head; //points at pointer ("next") pointing at current event
+
+	while (p)
+	{
+		if (p->component == comp) //remove
+		{
+			*pp = p->next; //change last 'next' pointer
+			delete p; //delete this block
+			p = *pp; //point at next block
+		}
+		else //keep, jump over
+		{
+			pp = &p->next; //change pointer-pointer to next 'next' pointer... ;-)
+			p = p->next; //change pointer to next block
+		}
+	}
+}
+
+void Event_Lists::Remove(Body *comp)
+{
+	Event_Lists *p = body_head;
+	Event_Lists **pp = &body_head;
+	while (p)
+	{
+		if (p->component == comp)
+		{
+			*pp = p->next;
+			delete p;
+			p = *pp;
+		}
+		else
+		{
+			pp = &p->next;
+			p = p->next;
+		}
+	}
+}
+
+void Event_Lists::Remove(Joint *comp)
+{
+	Event_Lists *p = joint_head;
+	Event_Lists **pp = &joint_head;
+	while (p)
+	{
+		if (p->component == comp)
+		{
+			*pp = p->next;
+			delete p;
+			p = *pp;
+		}
+		else
+		{
+			pp = &p->next;
+			p = p->next;
+		}
+	}
+}
+
--- src/events/event_lists.hpp
+++ src/events/event_lists.hpp
@@ -1,21 +1,40 @@
 #ifndef _RCX_EVENT_LISTS
 #define _RCX_EVENT_LISTS
 //definitions of components that causes/uses events
+#include "../shared/component.hpp"
 #include "../shared/geom.hpp"
 #include "../shared/body.hpp"
 #include "../shared/joint.hpp"
 
-template <typename T>
-class Event_List
+class Event_Lists
 {
 	public:
-		Event_List(T*); //for adding event
-		T *Get_Event(); //for reading event (and remove from list)
-		static void Remove(T*); //remove all events for component from this list
+		//for adding events
+		Event_Lists(Geom*);
+		Event_Lists(Body*);
+		Event_Lists(Joint*);
+
+		//for processing (read and remove) events
+		static bool Get_Event(Geom**);
+		static bool Get_Event(Body**);
+		static bool Get_Event(Joint**);
+
+		//for directly removing/ignoring events for a component
+		static void Remove(Geom*);
+		static void Remove(Body*);
+		static void Remove(Joint*);
+		//TODO: one function for removing from all lists? Maybe for future stuff
 
 	private:
-		static Event_List *head;
-		Event_List *next;
+		Component *component; //the one thing to keep track of
+
+		//different lists (by heads) for different components
+		static Event_Lists *geom_head;
+		static Event_Lists *body_head;
+		static Event_Lists *joint_head;
+
+		//next in list
+		Event_Lists *next;
 };
 
 #endif
